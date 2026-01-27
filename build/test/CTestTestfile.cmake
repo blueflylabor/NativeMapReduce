94badd6e175081286eeb6c624c8fa4f48d9f3757
藍翔技工 <48398805+blueflylabor@users.noreply.github.com>
2026-01-27 17:58:05 +0800

    fix unrecognizing function

--- build/test/CTestTestfile.cmake
+++ build/test/CTestTestfile.cmake
@@ -4,9 +4,9 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
+add_test(test_Queue "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/build/test/test_Queue")
+set_tests_properties(test_Queue PROPERTIES  RUN_SERIAL "ON" TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;37;add_test;/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;0;")
 add_test(test_StringUtils "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/build/test/test_StringUtils")
-set_tests_properties(test_StringUtils PROPERTIES  RUN_SERIAL "ON" TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;36;add_test;/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;0;")
-add_test(test_queue "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/build/test/test_queue")
-set_tests_properties(test_queue PROPERTIES  RUN_SERIAL "ON" TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;36;add_test;/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;0;")
-add_test(test_vector "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/build/test/test_vector")
-set_tests_properties(test_vector PROPERTIES  RUN_SERIAL "ON" TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;36;add_test;/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;0;")
+set_tests_properties(test_StringUtils PROPERTIES  RUN_SERIAL "ON" TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;37;add_test;/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;0;")
+add_test(test_Vector "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/build/test/test_Vector")
+set_tests_properties(test_Vector PROPERTIES  RUN_SERIAL "ON" TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;37;add_test;/Users/jeffwhynot/activity/CLionProjects/NativeMapReduce/test/CMakeLists.txt;0;")
