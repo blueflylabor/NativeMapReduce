8e164e5f231ca877173bf75586fa6afafe5e4dd3
藍翔技工 <48398805+blueflylabor@users.noreply.github.com>
2026-01-28 20:29:27 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ include/nmr/MapReduce.h
@@ -0,0 +1,37 @@
+#ifndef MAPREDUCE_TEST_H
+#define MAPREDUCE_TEST_H
+#include <iostream>
+#include <string>
+#include "nmr/Map.h"
+#include "nmr/Vector.h"
+#include "nmr/Queue.h"
+#include "nmr/StringUtils.h"
+
+namespace nmr {
+    template<typename K1, typename V1, typename K2, typename V2>
+    class Mapper {
+    public:
+        virtual ~Mapper() = default;
+        virtual void map(const K1& key, const V1& value, Map<K2, nmr::Vector<V2>>& intermediate) = 0;
+    };
+
+    template<typename K2, typename V2, typename V3>
+    class Reducer {
+        public:
+        virtual ~Reducer() = default;
+        virtual V3 reduce(const K2& key, const nmr::Vector<V2>& values) = 0;
+    };
+
+    class WordCountMapper : public Mapper<int, std::string, std::string, int> {
+        public:
+        void map(const int& lineNum, const string& line, nmr::Map<string, Vector<int>>& intermediate) override {
+            std::string cleanLine = nmr::StringUtils::removePunctuation(line);
+            nmr::Vector<std::string> words;
+            nmr::StringUtils::splitToStringArray(cleanLine, " ", words);
+            for (size_t i = 0; i < words.getSize(); ++i)
+                intermediate[words[i]].push_back(1);
+        }
+    };
+}
+
+#endif
---
+++ test/test_MapReduce.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Jeff Whynot on 2026/1/28.
+//
+
+#include "nmr/MapReduce.h"
+
+int main() {
+    return 0;
+}
